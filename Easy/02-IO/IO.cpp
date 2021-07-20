aac57e69e49d6d8911c276874e3b0ac0d39589cb
Vinicius Mylonas <53568949+vinimyls@users.noreply.github.com>
2021-07-20 09:10:30 -0300

    Create IO.cpp

---
+++ Easy/02-IO/IO.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a,b,c, total;
+    cin >> a >> b >> c;
+    total = a + b + c;
+    cout << total << endl;
+    return 0;
+}
